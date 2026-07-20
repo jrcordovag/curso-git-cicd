d4f5e1a7de3b5527305300a7395d971cd9169042
Roel Cordova <jrcordovag@gmail.com>
2026-07-20 13:09:06 -0600

    test: agregar pruebas unitarias e integrarlas al pipeline de CI

--- script.sh
+++ script.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 
-echo "Mensaje definitivo: combinamos master y feature con exito"
+sumar() {
+	local num1=$1
+	local num2=$2
+	echo $(($num1 + $num2))
+}
+
+if [ $# = 2 ]; then
+	sumar $1 $2
+fi
+
 
 
---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "=== Ejecutando pruebas unitarias ==="
+
+RESULTADO=$(./script.sh 2 3)
+
+if [ $RESULTADO -eq 5 ]; then
+	echo "Prueba 1 pasada: 2 + 3 = 5"
+else
+	echo "Prueba 1 fallo, se esperaba 5 pero se obtuvo '$RESULTADO'"
+	exit 1
+fi
+
+echo "=== Todas las pruebas se ejecutaron con exito ==="
+exit 0
+
