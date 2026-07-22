161c74cf81d012c3e44abbfec6424b90a98e9567
Roel Cordova <jrcordovag@gmail.com>
2026-07-22 09:39:20 -0600

    feat: agregar funcion reporte de estado y sus pruebas

--- test.sh
+++ test.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-echo "=== Ejecutando pruebas unitarias ==="
+echo "=== Ejecutando Pruebas Unitarias ==="
 
-RESULTADO=$(./script.sh 2 3)
-
-if [ "$RESULTADO" -eq 5 ]; then
-	echo "Prueba 1 pasada: 2 + 3 = 5"
+# Prueba 1: Suma
+RESULTADO_SUMA=$(./script.sh sumar 2 3)
+if [ "$RESULTADO_SUMA" -eq 5 ]; then
+    echo "✔ Prueba 1 pasada: 2 + 3 = 5"
 else
-	echo "Prueba 1 fallo, se esperaba 5 pero se obtuvo '$RESULTADO'"
-	exit 1
+    echo "✖ Prueba 1 falló: Se esperaba 5 pero se obtuvo '$RESULTADO_SUMA'"
+    exit 1
 fi
 
-echo "=== Todas las pruebas se ejecutaron con exito ==="
-exit 0
+# Prueba 2: Generar Reporte OK
+RESULTADO_REPORTE=$(./script.sh reporte ok)
+if [ "$RESULTADO_REPORTE" = "SISTEMA_OK" ]; then
+    echo "✔ Prueba 2 pasada: Reporte OK funcional"
+else
+    echo "✖ Prueba 2 falló: Se esperaba 'SISTEMA_OK' pero se obtuvo '$RESULTADO_REPORTE'"
+    exit 1
+fi
 
+echo "=== Todas las pruebas pasaron con éxito ==="
+exit 0
